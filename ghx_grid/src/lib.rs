9055d3ddbcb43b10639fc4e23dcd5361225456e2
Henauxg <19689618+Henauxg@users.noreply.github.com>
2024-03-06 13:55:17 +0100

    docs(ghx_grid): Fix missing doc at root level

--- ghx_grid/src/lib.rs
+++ ghx_grid/src/lib.rs
@@ -1,10 +1,12 @@
 #![warn(missing_docs)]
 
+//! This library provides utilities to manipulate 2d & 3d grid data
+
 /// Defines directions
 pub mod direction;
 
 /// Defines coordinate systems
 pub mod coordinate_system;
 
-// Defines grid structures
+/// Defines grid structures
 pub mod grid;
